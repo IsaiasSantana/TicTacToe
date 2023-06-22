599c4c736863af5f5a6f11650cc97dddb1b03d0b
Isaías Santana <isds.santana@gmail.com>
2023-06-21 22:45:23 -0300

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Isaías Santana on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
